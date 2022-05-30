8ef7abf76baf7a4290a131fb56a9594b4d6c70e9
PeterM8811 <pmlynarski88@gmail.com>
2022-05-30 18:20:40 +0200

    lesson version

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'angular-project-2022-pm-c61fd',
+    appId: '1:79829254654:web:c424d219df7cedf6b780af',
+    storageBucket: 'angular-project-2022-pm-c61fd.appspot.com',
+    apiKey: 'AIzaSyCItZIZjSVJsJzYR7biRxWYbmYM_QGFTsc',
+    authDomain: 'angular-project-2022-pm-c61fd.firebaseapp.com',
+    messagingSenderId: '79829254654',
+    measurementId: 'G-8DRQB8GND4',
+  },
   production: true,
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'angular-project-2022-pm-c61fd',
+    appId: '1:79829254654:web:c424d219df7cedf6b780af',
+    storageBucket: 'angular-project-2022-pm-c61fd.appspot.com',
+    apiKey: 'AIzaSyCItZIZjSVJsJzYR7biRxWYbmYM_QGFTsc',
+    authDomain: 'angular-project-2022-pm-c61fd.firebaseapp.com',
+    messagingSenderId: '79829254654',
+    measurementId: 'G-8DRQB8GND4',
+  },
   production: false,
 };
 
